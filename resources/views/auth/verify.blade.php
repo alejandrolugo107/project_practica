b669dffd8b52c2796b15858c8b74bf56cee397dc
Alejandro Lugo <alexlugo0410@gmail.com>
2022-05-22 23:59:48 -0500

    FirstCommit

---
+++ resources/views/auth/verify.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Validar tu correo electrónico') }}</div>
+
+                <div class="card-body">
+                    @if (session('resent'))
+                        <div class="alert alert-success" role="alert">
+                            {{ __('Un link de verificación se ha enviado a tu buzón de correo.') }}
+                        </div>
+                    @endif
+
+                    {{ __('Antes de continuar, revisa que hayas recibido el link en tu correo.') }}
+                    {{ __('Si no lo has recibido') }},
+                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                        @csrf
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
